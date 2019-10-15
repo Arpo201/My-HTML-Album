d33411aefd2f05c2d1e917c08038831b3048c33c
Arpo <arpo201@gmail.com>
2019-10-15 22:24:40 +0700

    Update 2.5

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Website_Test
  Practice to build the Website.
+
+Math Reference: http://www.tutormathphysics.com/index.php/component/content/article/180-math-m5-1-no3-vector/1393-math-m5-1-no3-vector-01.html
--- process.js
+++ process.js
@@ -5,20 +5,46 @@ function loveYou(){
     document.getElementById("love").value = answer;
     return false
 }
-function plusCss(){
+// Calculation page
+function plusJs(){
     var a, b, c;
-    a = Number(document.forms["plusForms"]["num_1"].value);
-    b = Number(document.forms["plusForms"]["num_2"].value);
+    a = Number(document.forms["plusForms"]["numAPlus"].value);
+    b = Number(document.forms["plusForms"]["numBPlus"].value);
     c = a+b;
     document.getElementById("plus").innerHTML = c;
     return false
 }
-function binary(){
-    var num = 69
-    var str = ""
-    while (num != 1,0){
-        str += num%2
-        num /= 2;
-    console.log(str);
-    }
+function minusJs(){
+    var a, b, c;
+    a = Number(document.forms["minusForms"]["numAMinus"].value);
+    b = Number(document.forms["minusForms"]["numBMinus"].value);
+    c = a-b;
+    document.getElementById("minus").innerHTML = c;
+    return false
+}
+
+//Vector 2D size
+function vector2D(){
+    var pointX1, pointY1, pointX2, pointY2, size
+    pointX1 = Number(document.forms["vertor2DForms"]["pointx1"].value)
+    pointY1 = Number(document.forms["vertor2DForms"]["pointy1"].value)
+    pointX2 = Number(document.forms["vertor2DForms"]["pointx2"].value)
+    pointY2 = Number(document.forms["vertor2DForms"]["pointy2"].value)
+    size = ((pointX1-pointX2)**2 + (pointY1-pointY2)**2)**0.5
+    document.getElementById("vector_size2D").innerHTML = size
+    return false
+}
+
+//Vector 3D size
+function vector3D(){
+    var pointX1, pointY1, pointZ1, pointX2, pointY2, pointZ2, size
+    pointX1 = Number(document.forms["vertor3DForms"]["pointx1"].value)
+    pointY1 = Number(document.forms["vertor3DForms"]["pointy1"].value)
+    pointZ1 = Number(document.forms["vertor3DForms"]["pointz1"].value)
+    pointX2 = Number(document.forms["vertor3DForms"]["pointx2"].value)
+    pointY2 = Number(document.forms["vertor3DForms"]["pointy2"].value)
+    pointZ2 = Number(document.forms["vertor3DForms"]["pointz2"].value)
+    size = ((pointX1-pointX2)**2 + (pointY1-pointY2)**2 + (pointZ1-pointZ2)**2)**0.5
+    document.getElementById("vector_size3D").innerHTML = size
+    return false
 }
